7b51c79421c9336ade90cf6b3e7463532e6004db
artsoroka <art.soroka@yandex.ru>
2014-04-12 14:43:36 +0000

    created AuthController

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+class AuthController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function showLoginForm(){
+		return View::make('auth.loginform'); 
+	}
+
+	public function authenticateUser(){
+
+		$userdata = array(
+	    	'login'     => Input::get('login'),
+	        'password'  => Input::get('password')
+	    );
+
+		if( Auth::attempt($userdata) ) {
+			return "welcome"; 
+		} else {
+			return View::make('auth.loginform', array(
+				'error' => 'user data is incorrect',
+				'old_input' => Input::all()
+			)); 
+		}
+
+	}
+
+	public function whoami(){
+		return Auth::user(); 
+	}
+
+	public function logout(){
+		Auth::logout();
+		return Redirect::to('/');
+	}
+
+}
