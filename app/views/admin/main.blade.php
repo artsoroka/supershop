f60446f03cddd4c3f125994d0f4b0cff2675a912
artsoroka <art.soroka@yandex.ru>
2014-04-13 13:45:16 +0000

    modified dashboard layout added links to Categories and Products

--- app/views/admin/main.blade.php
+++ app/views/admin/main.blade.php
@@ -7,5 +7,10 @@
 <body>
 	{{{ ( isset($error) )? $error : '' }}}
 	Welcome, {{{ Auth::user()->username }}}
+<br>
+<br>
+<a href="/admin/categories">Categories</a>
+<br>
+<a href="/admin/products">Products</a>
 </body>
 </html>
